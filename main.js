ab54828a5a237171ef6120498d0237e69e0121e6
demon-monk <antdude@126.com>
2019-02-04 14:41:06 +0800

    ✨ Feat: load json data from electron side

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const { app, BrowserWindow } = require("electron");
 const fs = require("fs");
 const path = require("path");
-const url = require("url");
+const util = require("util");
+fs.readFile = util.promisify(fs.readFile);
 let mainWindow;
 app.on("ready", () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL("http://localhost:1234");
 
-  fs.readFile("./data.json", (err, data) => {
-    console.log(data);
-  });
+  let patients, bills;
+
+  fs.readFile("./bills.json")
+    .then(data => (bills = JSON.parse(data.toString())))
+    .then(() => fs.readFile("./patients.json"))
+    .then(data => (patients = JSON.parse(data.toString())))
+    .then(_ => {
+      console.log({ patients, bills });
+      mainWindow.webContents.send(
+        "data:loaded",
+        JSON.stringify({ patients, bills })
+      );
+    });
 });
