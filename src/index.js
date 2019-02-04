00048bd1deec167e86cf59b94e8a6551b03ced64
demon-monk <antdude@126.com>
2019-02-04 15:10:16 +0800

    ✨ Feat: router setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+const AppRouter = () => (
+  <Router>
+    <div>
+      <Route path="/" exact component={Home} />
+      <Route path="/new/" component={Home} />
+      <Route path="/add-success/" component={Home} />
+    </div>
+  </Router>
+);
 
-ReactDOM.render(<Home />, document.querySelector("#root"));
+ReactDOM.render(<AppRouter />, document.querySelector("#root"));
