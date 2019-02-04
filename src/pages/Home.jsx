183802b5431269a442c5ce9e2aa92063eab9a61f
demon-monk <antdude@126.com>
2019-02-04 14:49:13 +0800

    ✨ Feat: add home basic layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Input from "../components/Input";
-
+import "./home.less";
 export default class Home extends Component {
   render() {
     return (
       <div className="home-page">
-        <Input />
+        <form>
+          <Input />
+          <button type="submit">搜索</button>
+        </form>
       </div>
     );
   }
