bdeab74e3dc6847771c3cf3d3d9467bad33e9e20
demon-monk <antdude@126.com>
2019-02-04 14:19:19 +0800

    ✨ Feat: load react from elelctron

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const { app, BrowserWindow } = require("electron");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
 let mainWindow;
 app.on("ready", () => {
   mainWindow = new BrowserWindow({});
+  mainWindow.loadURL("http://localhost:1234");
+
+  fs.readFile("./data.json", (err, data) => {
+    console.log(data);
+  });
 });
